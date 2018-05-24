322b1a528b415c1e684045648da350b9b74a622a
Tsukatsuki Kanade <i@sukitsuki.com>
2018-05-24 17:59:28 +0700

    Add .env
    Signed-off-by: Tsukatsuki Kanade <i@sukitsuki.com>

--- main.go
+++ main.go
@@ -6,9 +6,13 @@ import (
 	"os"
 	"os/signal"
 	"syscall"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	if err := godotenv.Load(); err != nil {
+		Log.Fatal("Error loading .env file")
+	}
 	Log.Info("Start Started")
 	signalChan := make(chan os.Signal, 1)
 	signal.Notify(signalChan, syscall.SIGINT, syscall.SIGKILL, syscall.SIGTERM)
